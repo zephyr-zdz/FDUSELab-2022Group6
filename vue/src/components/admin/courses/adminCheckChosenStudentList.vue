f64ab7acec0c77b84736ee1a362a23f7883e1ecd
zhoudazhi <dazhi527@hotmail.com>
2022-05-19 14:14:42 +0800

    feat: 管理员查看课程选课学生名单

--- vue/src/components/admin/courses/adminCheckChosenStudentList.vue
+++ vue/src/components/admin/courses/adminCheckChosenStudentList.vue
@@ -4,27 +4,24 @@
             stripe
             pager="page">
     <el-table-column
-      prop="stunum"
       label="学号"
       width="200">
       <template v-slot="scope">
-        <span>{{ scope.row.stunum }}</span>
+        <span>{{ scope.row.student.stunum }}</span>
       </template>
     </el-table-column>
     <el-table-column
-      prop="name"
       label="姓名"
       width="200">
       <template v-slot="scope">
-        <span>{{ scope.row.name }}</span>
+        <span>{{ scope.row.student.name }}</span>
       </template>
     </el-table-column>
     <el-table-column
-      prop="major"
       label="专业"
       >
       <template v-slot="scope">
-        <span>{{ scope.row.major }}</span>
+        <span>{{ scope.row.student.major }}</span>
       </template>
     </el-table-column>
   </el-table>
@@ -35,7 +32,27 @@ export default {
   name: 'adminCheckChosenStudentList',
   data () {
     return {
+      courseid: 0,
       chosenStudentTable: []
+    }
+  },
+  mounted () {
+    this.getChosenStudentList()
+  },
+  methods: {
+    getChosenStudentList () {
+      this.$axios.get('/api/admin/student-course/student-list', {
+        params: {
+          courseid: this.courseid
+        }
+      })
+        .then(response => {
+          this.chosenStudentTable = response.data.data
+          console.log(this.chosenStudentTable)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
